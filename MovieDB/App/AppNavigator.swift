914961870672568f9396e19427cf8e2e68e18512
Mohammad Azri Khairuddin <mohammad.azri@visionet.co.id>
2026-06-24 16:24:52 +0700

    Scaffold app architecture and data layer

---
+++ MovieDB/App/AppNavigator.swift
@@ -0,0 +1,29 @@
+//
+//  AppNavigator.swift
+//  MovieDB
+//
+//  Created by Mohammad Azri Khairuddin on 24/06/26.
+//
+
+import UIKit
+
+protocol Navigator {
+    // TODO:
+}
+
+class AppNavigator {
+    
+    private let screenFactory: ScreenFactory
+    
+    init(screenFactory: ScreenFactory) {
+        self.screenFactory = screenFactory
+    }
+    
+    func createInitialScreen() -> UINavigationController {
+        UINavigationController(rootViewController: screenFactory.createHomeScreen())
+    }
+}
+
+extension AppNavigator: Navigator {
+    // TODO:
+}
